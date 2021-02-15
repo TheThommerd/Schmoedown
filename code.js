c91dd35908bdfd34a79f322957d7870e95f9f61d
TheThommerd <79095774+TheThommerd@users.noreply.github.com>
2021-02-15 16:36:28 +0100

    Update code.js

--- code.js
+++ code.js
@@ -7,6 +7,7 @@
 		document.getElementById("time001").style.visibility = "hidden";
 		document.getElementById("time001").style.display = "none";
 		document.getElementById("q2").focus();
+                document.body.style.zoom="120%"
 
 function load(){
   	player001.innerHTML = document.getElementById("naam").value.toUpperCase();

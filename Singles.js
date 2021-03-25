5c86921a682d8ef8246bcd104e02da47ce95c80c
TheThommerd <79095774+TheThommerd@users.noreply.github.com>
2021-03-25 16:01:13 +0100

    Update Singles.js

--- Singles.js
+++ Singles.js
@@ -789,13 +789,13 @@ function submitname () {
   	record001.innerHTML = document.getElementById("wins").value + "-" + document.getElementById("losses").value;
 	document.getElementById("skip").style.visibility = "hidden";
 	document.getElementById("skip").style.display = "none";
-message004.innerHTML = "<option value=Alba>Greg Alba</option><select id=select><option value=Dagnino>Tom Dagnino</option><option value=Dewberry>Eliot Dewberry</option><option value=Macuga>Josh Macuga</option><option value=Witt>Adam Witt</option></select></br></br><button id=choose onclick=choosecomp()>Choose Opponent</button>";
+message004.innerHTML = "<select id=select><option value=Alba>Greg Alba</option><option value=Dagnino>Tom Dagnino</option><option value=Dewberry>Eliot Dewberry</option><option value=Macuga>Josh Macuga</option><option value=Witt>Adam Witt</option></select></br></br><button id=choose onclick=choosecomp()>Choose Opponent</button>";
 	message001.innerHTML = "Choose your opponent.";
 }
 function skip () {
 	document.getElementById("skip").style.visibility = "hidden";
 	document.getElementById("skip").style.display = "none";
-message004.innerHTML = "<option value=Alba>Greg Alba</option><select id=select><option value=Dagnino>Tom Dagnino</option><option value=Dewberry>Eliot Dewberry</option><option value=Macuga>Josh Macuga</option><option value=Witt>Adam Witt</option></select></br></br><button id=choose onclick=choosecomp()>Choose Opponent</button>";
+message004.innerHTML = "<select id=select><option value=Alba>Greg Alba</option><option value=Dagnino>Tom Dagnino</option><option value=Dewberry>Eliot Dewberry</option><option value=Macuga>Josh Macuga</option><option value=Witt>Adam Witt</option></select></br></br><button id=choose onclick=choosecomp()>Choose Opponent</button>";
 	message001.innerHTML = "Choose your opponent.";
 }
  	var a = 0;

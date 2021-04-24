f19c5ac23dd3735b91f55f1d7521c0c033c6eb9b
TheThommerd <79095774+TheThommerd@users.noreply.github.com>
2021-04-24 17:58:11 +0200

    Update Free4All_Bib.js

--- Free4All_Bib.js
+++ Free4All_Bib.js
@@ -2139,6 +2139,11 @@ if (question001[0].ques.length > 125){
         }
 
 function Challenge(){
+	     var question1 = document.getElementById("q1").value;
+if (question1.length < 1){
+            message003.innerHTML = "You can't challenge because you didn't submit an answer!";
+}
+if (question1.length >= 1){
 if (h == 1) {
 	    document.getElementById("chalacc").style.visibility = "visible";
 	    document.getElementById("chalden").style.visibility = "visible";
@@ -2146,6 +2151,7 @@ if (h == 1) {
  else if (h < 1) {
             message003.innerHTML = "No challenges left!";
 	    }
+}
 }
 
 function ChallengeAccepted(){

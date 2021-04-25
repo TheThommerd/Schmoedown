7acaaeea23d3a70cdcce416f8120e1b2e12e83a7
TheThommerd <79095774+TheThommerd@users.noreply.github.com>
2021-04-25 12:36:35 +0200

    Update IGQuarter.js

--- IGQuarter.js
+++ IGQuarter.js
@@ -465,12 +465,14 @@ function submitname () {
 	document.getElementById("skip").style.display = "none";
 message004.innerHTML = "<select id=select><option value=Damon>Alex Damon</option><option value=Ellison>Chance Ellison</option><option value=Inman>Jason Inman</option></select></br></br><button id=choose onclick=choosecomp()>Choose Opponent</button></div>";
 	message001.innerHTML = "Choose your opponent.";
+		document.getElementById("save").click();
 }
 function skip () {
 	document.getElementById("skip").style.visibility = "hidden";
 	document.getElementById("skip").style.display = "none";
 	message001.innerHTML = "Choose your opponent.";
 message004.innerHTML = "<select id=select><option value=Damon>Alex Damon</option><option value=Ellison>Chance Ellison</option><option value=Inman>Jason Inman</option></select></br></br><button id=choose onclick=choosecomp()>Choose Opponent</button></div>";
+		document.getElementById("save").click();
 }
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex ;

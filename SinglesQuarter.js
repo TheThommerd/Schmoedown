b204a204390b4a7c5cb112a10d139904abd75745
TheThommerd <79095774+TheThommerd@users.noreply.github.com>
2021-04-25 12:49:07 +0200

    Update SinglesQuarter.js

--- SinglesQuarter.js
+++ SinglesQuarter.js
@@ -816,12 +816,14 @@ function submitname () {
 	document.getElementById("skip").style.display = "none";
 message004.innerHTML = "<select id=select><option value=Andreyko>Marc Andreyko</option><option value=Kalinowski>Mike Kalinowski</option><option value=Meyer>Brendan Meyer</option><option value=Reilly>Mark Reilly</option><option value=Sneider>Jeff Sneider</option></select></br></br><button id=choose onclick=choosecomp()>Choose Opponent</button>";
 	message001.innerHTML = "Choose your opponent.";
+			document.getElementById("save").click();
 }
 function skip () {
 	document.getElementById("skip").style.visibility = "hidden";
 	document.getElementById("skip").style.display = "none";
 message004.innerHTML = "<select id=select><option value=Andreyko>Marc Andreyko</option><option value=Kalinowski>Mike Kalinowski</option><option value=Meyer>Brendan Meyer</option><option value=Reilly>Mark Reilly</option><option value=Sneider>Jeff Sneider</option></select></br></br><button id=choose onclick=choosecomp()>Choose Opponent</button>";
 	message001.innerHTML = "Choose your opponent.";
+			document.getElementById("save").click();
 }
  	var a = 0;
         a++;

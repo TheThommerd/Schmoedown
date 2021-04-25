cbe8ec54076a727767933ec2bff09d388a9495e2
TheThommerd <79095774+TheThommerd@users.noreply.github.com>
2021-04-25 15:44:20 +0200

    Update IG.js

--- IG.js
+++ IG.js
@@ -1116,9 +1116,6 @@ if (mc == 0){
 }
 if (mc != 0){
 	    document.getElementById("message004").style.top = "-140px";
-	   quescount++;
-	   quescor++;
-	   posspoints+=1;
 	    document.getElementById("JTE").style.visibility = "hidden";
             window.clearInterval(update);
             c = "-";
@@ -1271,8 +1268,6 @@ message006.innerHTML = "Question was stolen by " +competitorlist[0].lowercase;
 }
 else if (mc != 0){
 	    document.getElementById("message004").style.top = "-140px";
-	   quescount++;
-	   posspoints+=1;
 if (mc == 1){
 	    document.getElementById("JTE").style.visibility = "hidden";
             window.clearInterval(update);

c0d992c36acb092f5333e0fae35cc629b4133631
TheThommerd <79095774+TheThommerd@users.noreply.github.com>
2021-04-26 11:37:49 +0200

    Update SinglesR3.js

--- SinglesR3.js
+++ SinglesR3.js
@@ -816,14 +816,12 @@ function submitname () {
 	document.getElementById("skip").style.display = "none";
 message004.innerHTML = "<select id=select><option value=Howard>Stacy Howard</option><option value=McKee>Marisol McKee</option><option value=Preston>Paul Preston</option><option value=Seibold>Witney Seibold</option><option value=Zipper>Eric Zipper</option></select></br></br><button id=choose onclick=choosecomp()>Choose Opponent</button>";
 	message001.innerHTML = "Choose your opponent.";
-			document.getElementById("save").click();
 }
 function skip () {
 	document.getElementById("skip").style.visibility = "hidden";
 	document.getElementById("skip").style.display = "none";
 message004.innerHTML = "<select id=select><option value=Howard>Stacy Howard</option><option value=McKee>Marisol McKee</option><option value=Preston>Paul Preston</option><option value=Seibold>Witney Seibold</option><option value=Zipper>Eric Zipper</option></select></br></br><button id=choose onclick=choosecomp()>Choose Opponent</button>";
 	message001.innerHTML = "Choose your opponent.";
-			document.getElementById("save").click();
 }
  	var a = 0;
         a++;

e98839847311760e8d87cf49d76a9247712b31f9
TheThommerd <79095774+TheThommerd@users.noreply.github.com>
2021-07-05 13:31:39 +0200

    Update 1. Phantom Menace.js

--- SW/lib/1. Phantom Menace.js
+++ SW/lib/1. Phantom Menace.js
@@ -2,8 +2,8 @@
 		var incmusic = document.getElementById("myAudio2"); 
 		document.getElementById("myAudio").volume = 0.1;
 		document.getElementById("myAudio2").volume = 0.05;
-                var wins = document.getElementById("wins").value;
-                var losses = document.getElementById("losses").value;
+                var wins = document.getElementById("wins_SW").value;
+                var losses = document.getElementById("losses_SW").value;
 	    	message004.innerHTML = "<input id=q2 type=text maxlength=15 placeholder=Name /><br /><br /><input id=q3 placeholder=@Twitter type=text maxlength=15 /><br /><br /><button id=submit3  class=buttons001 onclick=submitname()>Submit </button><br /><br /><button id=skip class=buttons001 onclick=skip()>Skip</button>";
 	    	document.getElementById("JTE").style.visibility = "hidden";
 		document.getElementById("time001").style.visibility = "hidden";

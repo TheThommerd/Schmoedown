5bb642ca2ddcb905bf9e2799670d5a5f13048992
TheThommerd <79095774+TheThommerd@users.noreply.github.com>
2021-07-08 13:52:39 +0200

    Update SWChamp.js

--- SW/SWChamp.js
+++ SW/SWChamp.js
@@ -6281,13 +6281,30 @@ if (speed == 1){
 	    document.getElementById("message005").style.display = "initial";
             window.clearInterval(update);       
 		var chance = Math.floor(Math.random() * 91) + (competitorlist[0].percentage / 10);
+                message001.innerHTML = speedquestion001[0].ques;
+	if (speedquestion001[0].ques.length < 50){
+if (chance < 20){t = 13; nobody = 1}
+else if (chance < 25){t = 8; nobody = 0}
+else if (chance < 30){t = 4; nobody = 0}
+else if (chance < 50){t = 3; nobody = 0}
+else if (chance < 100){t = 2; nobody = 0}
+}
+else if (speedquestion001[0].ques.length < 110){
+if (chance < 20){t = 14; nobody = 1}
+else if (chance < 25){t = 9; nobody = 0}
+else if (chance < 30){t = 4; nobody = 0}
+else if (chance < 55){t = 3; nobody = 0}
+else if (chance < 100){t = 2; nobody = 0}
+}
+else if (speedquestion001[0].ques.length >= 110){
 if (chance < 20){t = 15; nobody = 1}
 else if (chance < 25){t = 10; nobody = 0}
-else if (chance < 30){t = 4; nobody = 0}
-else if (chance < 60){t = 3; nobody = 0}
+else if (chance < 30){t = 6; nobody = 0}
+else if (chance < 50){t = 4; nobody = 0}
+else if (chance < 90){t = 3; nobody = 0}
 else if (chance < 100){t = 2; nobody = 0}
+}
 update = setInterval("speedtimer001()", 1000);
-                message001.innerHTML = speedquestion001[0].ques;
 if (speedquestion001[0].ques.length > 125){
 		document.getElementById("message001").style.fontSize = "70%";
 	document.getElementById("player001").style.top = "27px";
@@ -6317,13 +6334,30 @@ else if (speed == 2){
 	    document.getElementById("message005").style.display = "initial";
             window.clearInterval(update);
 		var chance = Math.floor(Math.random() * 91) + (competitorlist[0].percentage / 10);
+                message001.innerHTML = speedquestion001[1].ques;
+	if (speedquestion001[1].ques.length < 50){
+if (chance < 20){t = 13; nobody = 1}
+else if (chance < 25){t = 8; nobody = 0}
+else if (chance < 30){t = 4; nobody = 0}
+else if (chance < 50){t = 3; nobody = 0}
+else if (chance < 100){t = 2; nobody = 0}
+}
+else if (speedquestion001[1].ques.length < 110){
+if (chance < 20){t = 14; nobody = 1}
+else if (chance < 25){t = 9; nobody = 0}
+else if (chance < 30){t = 4; nobody = 0}
+else if (chance < 55){t = 3; nobody = 0}
+else if (chance < 100){t = 2; nobody = 0}
+}
+else if (speedquestion001[1].ques.length >= 110){
 if (chance < 20){t = 15; nobody = 1}
 else if (chance < 25){t = 10; nobody = 0}
-else if (chance < 30){t = 4; nobody = 0}
-else if (chance < 60){t = 3; nobody = 0}
+else if (chance < 30){t = 6; nobody = 0}
+else if (chance < 50){t = 4; nobody = 0}
+else if (chance < 90){t = 3; nobody = 0}
 else if (chance < 100){t = 2; nobody = 0}
+}
 update = setInterval("speedtimer001()", 1000);
-                message001.innerHTML = speedquestion001[1].ques;
 if (speedquestion001[1].ques.length > 125){
 		document.getElementById("message001").style.fontSize = "70%";
 	document.getElementById("player001").style.top = "27px";
@@ -6353,13 +6387,30 @@ else if (speed == 3){
 	    document.getElementById("message005").style.display = "initial";
             window.clearInterval(update);
 		var chance = Math.floor(Math.random() * 91) + (competitorlist[0].percentage / 10);
+                message001.innerHTML = speedquestion001[2].ques;
+	if (speedquestion001[2].ques.length < 50){
+if (chance < 20){t = 13; nobody = 1}
+else if (chance < 25){t = 8; nobody = 0}
+else if (chance < 30){t = 4; nobody = 0}
+else if (chance < 50){t = 3; nobody = 0}
+else if (chance < 100){t = 2; nobody = 0}
+}
+else if (speedquestion001[2].ques.length < 110){
+if (chance < 20){t = 14; nobody = 1}
+else if (chance < 25){t = 9; nobody = 0}
+else if (chance < 30){t = 4; nobody = 0}
+else if (chance < 55){t = 3; nobody = 0}
+else if (chance < 100){t = 2; nobody = 0}
+}
+else if (speedquestion001[2].ques.length >= 110){
 if (chance < 20){t = 15; nobody = 1}
 else if (chance < 25){t = 10; nobody = 0}
-else if (chance < 30){t = 4; nobody = 0}
-else if (chance < 60){t = 3; nobody = 0}
+else if (chance < 30){t = 6; nobody = 0}
+else if (chance < 50){t = 4; nobody = 0}
+else if (chance < 90){t = 3; nobody = 0}
 else if (chance < 100){t = 2; nobody = 0}
+}
 update = setInterval("speedtimer001()", 1000);
-                message001.innerHTML = speedquestion001[2].ques;
 if (speedquestion001[2].ques.length > 125){
 		document.getElementById("message001").style.fontSize = "70%";
 	document.getElementById("player001").style.top = "27px";
@@ -6389,13 +6440,30 @@ else if (speed == 4){
 	    document.getElementById("message005").style.display = "initial";
             window.clearInterval(update);
 		var chance = Math.floor(Math.random() * 91) + (competitorlist[0].percentage / 10);
+                message001.innerHTML = speedquestion001[3].ques;
+	if (speedquestion001[3].ques.length < 50){
+if (chance < 20){t = 13; nobody = 1}
+else if (chance < 25){t = 8; nobody = 0}
+else if (chance < 30){t = 4; nobody = 0}
+else if (chance < 50){t = 3; nobody = 0}
+else if (chance < 100){t = 2; nobody = 0}
+}
+else if (speedquestion001[3].ques.length < 110){
+if (chance < 20){t = 14; nobody = 1}
+else if (chance < 25){t = 9; nobody = 0}
+else if (chance < 30){t = 4; nobody = 0}
+else if (chance < 55){t = 3; nobody = 0}
+else if (chance < 100){t = 2; nobody = 0}
+}
+else if (speedquestion001[3].ques.length >= 110){
 if (chance < 20){t = 15; nobody = 1}
 else if (chance < 25){t = 10; nobody = 0}
-else if (chance < 30){t = 4; nobody = 0}
-else if (chance < 60){t = 3; nobody = 0}
+else if (chance < 30){t = 6; nobody = 0}
+else if (chance < 50){t = 4; nobody = 0}
+else if (chance < 90){t = 3; nobody = 0}
 else if (chance < 100){t = 2; nobody = 0}
+}
 update = setInterval("speedtimer001()", 1000);
-                message001.innerHTML = speedquestion001[3].ques;
 if (speedquestion001[3].ques.length > 125){
 		document.getElementById("message001").style.fontSize = "70%";
 	document.getElementById("player001").style.top = "27px";
@@ -6425,13 +6493,30 @@ else if (speed == 5){
 	    document.getElementById("message005").style.display = "initial";
             window.clearInterval(update);
 		var chance = Math.floor(Math.random() * 91) + (competitorlist[0].percentage / 10);
+                message001.innerHTML = speedquestion001[4].ques;
+	if (speedquestion001[4].ques.length < 50){
+if (chance < 20){t = 13; nobody = 1}
+else if (chance < 25){t = 8; nobody = 0}
+else if (chance < 30){t = 4; nobody = 0}
+else if (chance < 50){t = 3; nobody = 0}
+else if (chance < 100){t = 2; nobody = 0}
+}
+else if (speedquestion001[4].ques.length < 110){
+if (chance < 20){t = 14; nobody = 1}
+else if (chance < 25){t = 9; nobody = 0}
+else if (chance < 30){t = 4; nobody = 0}
+else if (chance < 55){t = 3; nobody = 0}
+else if (chance < 100){t = 2; nobody = 0}
+}
+else if (speedquestion001[4].ques.length >= 110){
 if (chance < 20){t = 15; nobody = 1}
 else if (chance < 25){t = 10; nobody = 0}
-else if (chance < 30){t = 4; nobody = 0}
-else if (chance < 60){t = 3; nobody = 0}
+else if (chance < 30){t = 6; nobody = 0}
+else if (chance < 50){t = 4; nobody = 0}
+else if (chance < 90){t = 3; nobody = 0}
 else if (chance < 100){t = 2; nobody = 0}
+}
 update = setInterval("speedtimer001()", 1000);
-                message001.innerHTML = speedquestion001[4].ques;
 if (speedquestion001[4].ques.length > 125){
 		document.getElementById("message001").style.fontSize = "70%";
 	document.getElementById("player001").style.top = "27px";

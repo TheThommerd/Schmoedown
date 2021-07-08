dc369eb43a6439eeb050e4a31fedb72d87ea5735
TheThommerd <79095774+TheThommerd@users.noreply.github.com>
2021-07-08 16:59:55 +0200

    Update IGSemi.js

--- IG/IGSemi.js
+++ IG/IGSemi.js
@@ -1531,6 +1531,8 @@ function lose(){
 var mc = 0;
 var open = 0;
 function checker() {
+	similarity1 = 0;
+similarity2 = 0;
 document.getElementById("message003").style.top = "-140px";
 document.getElementById("message004").style.top = "-140px";
 document.getElementById("message006").style.top = "-140px";
@@ -5695,6 +5697,8 @@ next005();
 }
 }
 function checkerq1(){
+	similarity1 = 0;
+similarity2 = 0;
 	    var stealer01 = document.getElementById("q1").value.toUpperCase();
 	    var stealer1 = myTrim(stealer01);
 	    stealer1 = myTrim(stealer1);
@@ -5757,6 +5761,8 @@ else if ((stealmc == 0) && (stealopp == 0)){
 }	   
 }
 function checkerq2(){
+	similarity1 = 0;
+similarity2 = 0;
 	    var stealer02 = document.getElementById("q1").value.toUpperCase();
 	    var stealer2 = myTrim(stealer02);
 	    stealer2 = myTrim(stealer2);
@@ -5819,6 +5825,8 @@ else if ((stealmc == 0) && (stealopp == 0)){
 }	   
 }
 function checkerq3(){
+	similarity1 = 0;
+similarity2 = 0;
 	    var stealer03 = document.getElementById("q1").value.toUpperCase();
 	    var stealer3 = myTrim(stealer03);
 	    stealer3 = myTrim(stealer3);
@@ -5881,6 +5889,8 @@ else if ((stealmc == 0) && (stealopp == 0)){
 }	   
 }
 function checkerq4(){
+	similarity1 = 0;
+similarity2 = 0;
 	    var stealer04 = document.getElementById("q1").value.toUpperCase();
 	    var stealer4 = myTrim(stealer04);
 	    stealer4 = myTrim(stealer4);
@@ -5943,6 +5953,8 @@ else if ((stealmc == 0) && (stealopp == 0)){
 }	   
 }
 function checkerq5(){
+	similarity1 = 0;
+similarity2 = 0;
 	    var stealer05 = document.getElementById("q1").value.toUpperCase();
 	    var stealer5 = myTrim(stealer05);
 	    stealer5 = myTrim(stealer5);

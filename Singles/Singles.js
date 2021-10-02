479bc8a1a2638f934572439f245453fd5601a9d2
TheThommerd <79095774+TheThommerd@users.noreply.github.com>
2021-10-02 16:54:42 +0200

    Add files via upload

--- Singles/Singles.js
+++ Singles/Singles.js
@@ -5552,7 +5552,7 @@ if (typeof(element4) != 'undefined' && element4 != null)
                 message005.innerHTML = "";
                 message006.innerHTML = "";
 		document.getElementById("qu002").style.display = "none";
- 		qu001.innerHTML = "STEAL";
+ 	        qu001.innerHTML = "STEAL - " +compcat[0].cat.toUpperCase();
 		open++;
                 if (c < 1) {
                     window.clearInterval(update);
@@ -5636,7 +5636,7 @@ if (typeof(element4) != 'undefined' && element4 != null)
                 message005.innerHTML = "";
                 message006.innerHTML = "";
 		document.getElementById("qu002").style.display = "none";
- 		qu001.innerHTML = "STEAL";
+ 	        qu001.innerHTML = "STEAL - " +compcat[1].cat.toUpperCase();
 		open++;
                 if (c < 1) {
                     window.clearInterval(update);
@@ -5721,7 +5721,7 @@ if (typeof(element4) != 'undefined' && element4 != null)
                 message005.innerHTML = "";
                 message006.innerHTML = "";
 		document.getElementById("qu002").style.display = "none";
- 		qu001.innerHTML = "STEAL";
+ 	        qu001.innerHTML = "STEAL - " +compcat[2].cat.toUpperCase();
 		open++;
                 if (c < 1) {
                     window.clearInterval(update);
@@ -5806,7 +5806,7 @@ if (typeof(element4) != 'undefined' && element4 != null)
                 message005.innerHTML = "";
                 message006.innerHTML = "";
 		document.getElementById("qu002").style.display = "none";
- 		qu001.innerHTML = "STEAL";
+ 	        qu001.innerHTML = "STEAL - " +compcat[3].cat.toUpperCase();
 		open++;
                 if (c < 1) {
                     window.clearInterval(update);
@@ -5910,7 +5910,7 @@ else if (hidechance < 100){document.getElementById('incop4').style.backgroundCol
                 message006.innerHTML = "";
                 message004.innerHTML = "";
 		document.getElementById("qu002").style.display = "none";
- 		qu001.innerHTML = "STEAL";
+ 	        qu001.innerHTML = "STEAL - " +compcat[4].cat.toUpperCase();
 		open = 0;
 		mc++;
                 if (c < 1) {
@@ -6021,7 +6021,7 @@ else if (hidechance < 100){document.getElementById('incop4').style.backgroundCol
                 message006.innerHTML = "";
                 message004.innerHTML = "";
 		document.getElementById("qu002").style.display = "none";
- 		qu001.innerHTML = "STEAL";
+ 	        qu001.innerHTML = "STEAL - " +compcat[5].cat.toUpperCase();
 		mc++;
                 if (c < 1) {
                     window.clearInterval(update);
@@ -6132,7 +6132,7 @@ else if (hidechance < 100){document.getElementById('incop4').style.backgroundCol
                 message006.innerHTML = "";
                 message004.innerHTML = "";
 		document.getElementById("qu002").style.display = "none";
- 		qu001.innerHTML = "STEAL";
+ 	        qu001.innerHTML = "STEAL - " +compcat[6].cat.toUpperCase();
 		mc++;
                 if (c < 1) {
                     window.clearInterval(update);
@@ -6243,7 +6243,7 @@ else if (hidechance < 100){document.getElementById('incop4').style.backgroundCol
                 message006.innerHTML = "";
                 message004.innerHTML = "";
 		document.getElementById("qu002").style.display = "none";
- 		qu001.innerHTML = "STEAL";
+ 	        qu001.innerHTML = "STEAL - " +compcat[7].cat.toUpperCase();
 		mc++;
                 if (c < 1) {
                     window.clearInterval(update);

595be1c84d85adf6ef902240747db9894bb133b7
TheThommerd <79095774+TheThommerd@users.noreply.github.com>
2021-10-17 12:33:31 +0200

    Update SWR3.js

--- SW/SWR3.js
+++ SW/SWR3.js
@@ -36,7 +36,7 @@ message001.innerHTML = "Enter your name and twitter, or press Skip to proceed.";
 else
 {
 message006.innerHTML = "Password incorrect!";
-document.getElementById("message006").style.top = "-140px";
+document.getElementById("message006").style.top = "-160px";
 }
 }
 function submitname () {
@@ -107,10 +107,10 @@ function winKO(){
 		document.getElementById("passwordsaverEight").value = "BIB FORTUNA";
 		document.getElementById("save").click();
 		message004.innerHTML = "You have unlocked the next round. Use the password 'Bib Fortuna' to enter." + "</br></br>" + "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
-		document.getElementById("message002").style.top = "-200px";
-		document.getElementById("message004").style.top = "-375px";
-		document.getElementById("message003").style.top = "-200px";
-		document.getElementById("message006").style.top = "-100px";
+		document.getElementById("message002").style.top = "-220px";
+		document.getElementById("message004").style.top = "-395px";
+		document.getElementById("message003").style.top = "-220px";
+		document.getElementById("message006").style.top = "-120px";
 		document.getElementById("message006").style.fontSize = "110%";
 }
 function loseKO(){
@@ -136,10 +136,10 @@ function loseKO(){
 		document.getElementById("KOd_SW").stepUp(1);
 		document.getElementById("save").click();
             	message004.innerHTML = "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
-		document.getElementById("message002").style.top = "-200px";
-		document.getElementById("message004").style.top = "-375px";
-		document.getElementById("message003").style.top = "-200px";
-		document.getElementById("message006").style.top = "-100px";
+		document.getElementById("message002").style.top = "-220px";
+		document.getElementById("message004").style.top = "-395px";
+		document.getElementById("message003").style.top = "-220px";
+		document.getElementById("message006").style.top = "-120px";
 		document.getElementById("message006").style.fontSize = "110%";
 }
 function winTKO(){
@@ -166,10 +166,10 @@ function winTKO(){
 		document.getElementById("passwordsaverEight").value = "BIB FORTUNA";
 		document.getElementById("save").click();
 		message004.innerHTML = "You have unlocked the next round. Use the password 'Bib Fortuna' to enter." + "</br></br>" + "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
-		document.getElementById("message002").style.top = "-200px";
-		document.getElementById("message004").style.top = "-375px";
-		document.getElementById("message003").style.top = "-200px";
-		document.getElementById("message006").style.top = "-100px";
+		document.getElementById("message002").style.top = "-220px";
+		document.getElementById("message004").style.top = "-395px";
+		document.getElementById("message003").style.top = "-220px";
+		document.getElementById("message006").style.top = "-120px";
 		document.getElementById("message006").style.fontSize = "110%";
 }
 function loseTKO(){
@@ -195,10 +195,10 @@ function loseTKO(){
 		document.getElementById("TKOd_SW").stepUp(1);
 		document.getElementById("save").click();
             	message004.innerHTML = "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
-		document.getElementById("message002").style.top = "-200px";
-		document.getElementById("message004").style.top = "-375px";
-		document.getElementById("message003").style.top = "-200px";
-		document.getElementById("message006").style.top = "-100px";
+		document.getElementById("message002").style.top = "-220px";
+		document.getElementById("message004").style.top = "-395px";
+		document.getElementById("message003").style.top = "-220px";
+		document.getElementById("message006").style.top = "-120px";
 		document.getElementById("message006").style.fontSize = "110%";
 }
 function win(){
@@ -224,10 +224,10 @@ function win(){
 		document.getElementById("passwordsaverEight").value = "BIB FORTUNA";
 		document.getElementById("save").click();
 		message004.innerHTML = "You have unlocked the next round. Use the password 'Bib Fortuna' to enter." + "</br></br>" + "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
-		document.getElementById("message002").style.top = "-200px";
-		document.getElementById("message004").style.top = "-375px";
-		document.getElementById("message003").style.top = "-200px";
-		document.getElementById("message006").style.top = "-100px";
+		document.getElementById("message002").style.top = "-220px";
+		document.getElementById("message004").style.top = "-395px";
+		document.getElementById("message003").style.top = "-220px";
+		document.getElementById("message006").style.top = "-120px";
 		document.getElementById("message006").style.fontSize = "110%";
 }
 function lose(){
@@ -252,10 +252,10 @@ function lose(){
 		document.getElementById("losses_SW").stepUp(1);
 		document.getElementById("save").click();
             	message004.innerHTML = "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
-		document.getElementById("message002").style.top = "-200px";
-		document.getElementById("message004").style.top = "-375px";
-		document.getElementById("message003").style.top = "-200px";
-		document.getElementById("message006").style.top = "-100px";
+		document.getElementById("message002").style.top = "-220px";
+		document.getElementById("message004").style.top = "-395px";
+		document.getElementById("message003").style.top = "-220px";
+		document.getElementById("message006").style.top = "-120px";
 		document.getElementById("message006").style.fontSize = "110%";
 }
      	$("#message004").keypress(function(event) { 	

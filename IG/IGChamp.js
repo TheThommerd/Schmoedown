75f1ecb84f3748a18647345ffa3bc958b24e3c1c
TheThommerd <79095774+TheThommerd@users.noreply.github.com>
2021-10-18 20:13:55 +0200

    Update IGChamp.js

--- IG/IGChamp.js
+++ IG/IGChamp.js
@@ -1524,10 +1524,10 @@ function winKO(){
 		document.getElementById("TotalPoints_IG").value = parseInt(document.getElementById("TotalPoints_IG").value) +  b
 		document.getElementById("save").click();
             	message004.innerHTML = "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
-		document.getElementById("message002").style.top = "-220px";
-		document.getElementById("message004").style.top = "-395px";
-		document.getElementById("message003").style.top = "-220px";
-		document.getElementById("message006").style.top = "-120px";
+		document.getElementById("message002").style.top = "-260px";
+		document.getElementById("message004").style.top = "-435px";
+		document.getElementById("message003").style.top = "-260px";
+		document.getElementById("message006").style.top = "-160px";
 		document.getElementById("message006").style.fontSize = "110%";
 }
 function loseKO(){
@@ -1557,10 +1557,10 @@ function loseKO(){
 		document.getElementById("TotalPoints_IG").value = parseInt(document.getElementById("TotalPoints_IG").value) +  b
 		document.getElementById("save").click();
             	message004.innerHTML = "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
-		document.getElementById("message002").style.top = "-220px";
-		document.getElementById("message004").style.top = "-395px";
-		document.getElementById("message003").style.top = "-220px";
-		document.getElementById("message006").style.top = "-120px";
+		document.getElementById("message002").style.top = "-260px";
+		document.getElementById("message004").style.top = "-435px";
+		document.getElementById("message003").style.top = "-260px";
+		document.getElementById("message006").style.top = "-160px";
 		document.getElementById("message006").style.fontSize = "110%";
 }
 function winTKO(){
@@ -1591,10 +1591,10 @@ function winTKO(){
 		document.getElementById("TotalPoints_IG").value = parseInt(document.getElementById("TotalPoints_IG").value) +  b
 		document.getElementById("save").click();
             	message004.innerHTML = "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
-		document.getElementById("message002").style.top = "-220px";
-		document.getElementById("message004").style.top = "-395px";
-		document.getElementById("message003").style.top = "-220px";
-		document.getElementById("message006").style.top = "-120px";
+		document.getElementById("message002").style.top = "-260px";
+		document.getElementById("message004").style.top = "-435px";
+		document.getElementById("message003").style.top = "-260px";
+		document.getElementById("message006").style.top = "-160px";
 		document.getElementById("message006").style.fontSize = "110%";
 }
 function loseTKO(){
@@ -1624,10 +1624,10 @@ function loseTKO(){
 		document.getElementById("TotalPoints_IG").value = parseInt(document.getElementById("TotalPoints_IG").value) +  b
 		document.getElementById("save").click();
             	message004.innerHTML = "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
-		document.getElementById("message002").style.top = "-220px";
-		document.getElementById("message004").style.top = "-395px";
-		document.getElementById("message003").style.top = "-220px";
-		document.getElementById("message006").style.top = "-120px";
+		document.getElementById("message002").style.top = "-260px";
+		document.getElementById("message004").style.top = "-435px";
+		document.getElementById("message003").style.top = "-260px";
+		document.getElementById("message006").style.top = "-160px";
 		document.getElementById("message006").style.fontSize = "110%";
 }
 function win(){
@@ -1657,10 +1657,10 @@ function win(){
 		document.getElementById("TotalPoints_IG").value = parseInt(document.getElementById("TotalPoints_IG").value) +  b
 		document.getElementById("save").click();
             	message004.innerHTML = "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
-		document.getElementById("message002").style.top = "-220px";
-		document.getElementById("message004").style.top = "-395px";
-		document.getElementById("message003").style.top = "-220px";
-		document.getElementById("message006").style.top = "-120px";
+		document.getElementById("message002").style.top = "-260px";
+		document.getElementById("message004").style.top = "-435px";
+		document.getElementById("message003").style.top = "-260px";
+		document.getElementById("message006").style.top = "-160px";
 		document.getElementById("message006").style.fontSize = "110%";
 }
 function lose(){
@@ -1689,10 +1689,10 @@ function lose(){
 		document.getElementById("TotalPoints_IG").value = parseInt(document.getElementById("TotalPoints_IG").value) +  b
 		document.getElementById("save").click();
             	message004.innerHTML = "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
-		document.getElementById("message002").style.top = "-220px";
-		document.getElementById("message004").style.top = "-395px";
-		document.getElementById("message003").style.top = "-220px";
-		document.getElementById("message006").style.top = "-120px";
+		document.getElementById("message002").style.top = "-260px";
+		document.getElementById("message004").style.top = "-435px";
+		document.getElementById("message003").style.top = "-260px";
+		document.getElementById("message006").style.top = "-160px";
 		document.getElementById("message006").style.fontSize = "110%";
 }
 var mc = 0;

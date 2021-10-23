0e44aa7fa1436a78e81dbf895f2f3c4c106e6f48
TheThommerd <79095774+TheThommerd@users.noreply.github.com>
2021-10-23 16:24:29 +0200

    Update ironman.js

--- Singles/ironman.js
+++ Singles/ironman.js
@@ -457,7 +457,7 @@ posspoints++;
 posspointsopp++;
 quescountopp++;
       		message003.innerHTML = "The correct answer is " + question001[vraag].answ2; 
-		    document.getElementById("message003").style.top = "-210px";
+		    document.getElementById("message003").style.top = "-230px";
            	window.clearInterval(update);
                 time001.innerHTML = "Time's up!";
 		document.getElementById("time001").style.fontSize = "100%";

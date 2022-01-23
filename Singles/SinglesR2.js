4a630e538cefb9ba139c32d96a644d4cd67add4b
TheThommerd <79095774+TheThommerd@users.noreply.github.com>
2022-01-23 19:28:29 +0100

    Update SinglesR2.js

--- Singles/SinglesR2.js
+++ Singles/SinglesR2.js
@@ -80,7 +80,8 @@ message004.innerHTML = "<select id=select><option value=Ghai>Andrew Ghai</option
 	message001.innerHTML = "Choose your opponent.";
 }
 function winKO(){
-	    	document.getElementById("JTE").style.visibility = "hidden";
+		document.getElementById("JTE").style.display = "none";
+		document.getElementById("time001").style.display = "none";
                 window.clearInterval(update);
                 c = "-";
                 message001.innerHTML = "End of Match";
@@ -107,14 +108,15 @@ function winKO(){
 document.getElementById("passwordsaverR3").value = "JANE FONDA";
 message004.innerHTML = "You have unlocked the next round. Use the password 'Jane Fonda' to enter." + "</br></br>" + "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
 		document.getElementById("save").click();
-		document.getElementById("message002").style.top = "-280px";
+		document.getElementById("message002").style.top = "-275px";
 		document.getElementById("message004").style.top = "-455px";
-		document.getElementById("message003").style.top = "-280px";
+		document.getElementById("message003").style.top = "-275px";
 		document.getElementById("message006").style.top = "-180px";
 		document.getElementById("message006").style.fontSize = "110%";
 }
 function loseKO(){
-	    	document.getElementById("JTE").style.visibility = "hidden";
+		document.getElementById("JTE").style.display = "none";
+		document.getElementById("time001").style.display = "none";
                 window.clearInterval(update);
                 c = "-";
                 message001.innerHTML = "End of Match";
@@ -140,14 +142,15 @@ function loseKO(){
 		document.getElementById("TotalPoints").value = parseInt(document.getElementById("TotalPoints").value) +  b;
 		document.getElementById("save").click();
 message004.innerHTML = "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
-		document.getElementById("message002").style.top = "-280px";
+		document.getElementById("message002").style.top = "-275px";
 		document.getElementById("message004").style.top = "-455px";
-		document.getElementById("message003").style.top = "-280px";
+		document.getElementById("message003").style.top = "-275px";
 		document.getElementById("message006").style.top = "-180px";
 		document.getElementById("message006").style.fontSize = "110%";
 }
 function winTKO(){
-	    	document.getElementById("JTE").style.visibility = "hidden";
+		document.getElementById("JTE").style.display = "none";
+		document.getElementById("time001").style.display = "none";
                 window.clearInterval(update);
                 c = "-";
                 message001.innerHTML = "End of Match";
@@ -174,14 +177,15 @@ function winTKO(){
 document.getElementById("passwordsaverR3").value = "JANE FONDA";
 message004.innerHTML = "You have unlocked the next round. Use the password 'Jane Fonda' to enter." + "</br></br>" + "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
 	document.getElementById("save").click();
-		document.getElementById("message002").style.top = "-280px";
+		document.getElementById("message002").style.top = "-275px";
 		document.getElementById("message004").style.top = "-455px";
-		document.getElementById("message003").style.top = "-280px";
+		document.getElementById("message003").style.top = "-275px";
 		document.getElementById("message006").style.top = "-180px";
 		document.getElementById("message006").style.fontSize = "110%";
 }
 function loseTKO(){
-	    	document.getElementById("JTE").style.visibility = "hidden";
+		document.getElementById("JTE").style.display = "none";
+		document.getElementById("time001").style.display = "none";
                 window.clearInterval(update);
                 c = "-";
                 message001.innerHTML = "End of Match";
@@ -207,14 +211,15 @@ function loseTKO(){
 		document.getElementById("TotalPoints").value = parseInt(document.getElementById("TotalPoints").value) +  b;
 		document.getElementById("save").click();
 message004.innerHTML = "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
-		document.getElementById("message002").style.top = "-280px";
+		document.getElementById("message002").style.top = "-275px";
 		document.getElementById("message004").style.top = "-455px";
-		document.getElementById("message003").style.top = "-280px";
+		document.getElementById("message003").style.top = "-275px";
 		document.getElementById("message006").style.top = "-180px";
 		document.getElementById("message006").style.fontSize = "110%";
 }
 function win(){
-	    	document.getElementById("JTE").style.visibility = "hidden";
+		document.getElementById("JTE").style.display = "none";
+		document.getElementById("time001").style.display = "none";
                 window.clearInterval(update);
                 c = "-";
                 message001.innerHTML = "End of Match";
@@ -240,14 +245,15 @@ function win(){
 document.getElementById("passwordsaverR3").value = "JANE FONDA";
 message004.innerHTML = "You have unlocked the next round. Use the password 'Jane Fonda' to enter." + "</br></br>" + "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
 	document.getElementById("save").click();
-		document.getElementById("message002").style.top = "-280px";
+		document.getElementById("message002").style.top = "-275px";
 		document.getElementById("message004").style.top = "-455px";
-		document.getElementById("message003").style.top = "-280px";
+		document.getElementById("message003").style.top = "-275px";
 		document.getElementById("message006").style.top = "-180px";
 		document.getElementById("message006").style.fontSize = "110%";
 }
 function lose(){
-	    	document.getElementById("JTE").style.visibility = "hidden";
+		document.getElementById("JTE").style.display = "none";
+		document.getElementById("time001").style.display = "none";
                 window.clearInterval(update);
                 c = "-";
                 message001.innerHTML = "End of Match";
@@ -272,9 +278,9 @@ function lose(){
 		document.getElementById("TotalPoints").value = parseInt(document.getElementById("TotalPoints").value) +  b;
 		document.getElementById("save").click();
 message004.innerHTML = "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
-		document.getElementById("message002").style.top = "-280px";
+		document.getElementById("message002").style.top = "-275px";
 		document.getElementById("message004").style.top = "-455px";
-		document.getElementById("message003").style.top = "-280px";
+		document.getElementById("message003").style.top = "-275px";
 		document.getElementById("message006").style.top = "-180px";
 		document.getElementById("message006").style.fontSize = "110%";
 }

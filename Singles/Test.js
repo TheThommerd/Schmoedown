9da6161ac47b6b9ce4838550fc707e34449f3906
TheThommerd <79095774+TheThommerd@users.noreply.github.com>
2022-01-23 14:57:47 +0100

    Add files via upload

--- Singles/Test.js
+++ Singles/Test.js
@@ -4707,13 +4707,13 @@ document.getElementById("box10").checked = true;
 document.getElementById("box10").disabled = true;
 }
 $('input[type=checkbox]').on('change', function (e) {
-    if ($('input[type=checkbox]:checked').length > 8) {
+    if ($('input[type=checkbox]:checked').length > 6) {
         $(this).prop('checked', false);
     }
-    if ($('input[type=checkbox]:checked').length == 8) {
+    if ($('input[type=checkbox]:checked').length == 6) {
 	document.getElementById("confirm").style.visibility= "visible"; 
     }
-    if ($('input[type=checkbox]:checked').length < 8) {
+    if ($('input[type=checkbox]:checked').length < 6) {
 	document.getElementById("confirm").style.visibility= "hidden"; 
     }
 });

a3f12663e48a2e479702032f2e0a942bf68f6c87
TheThommerd <79095774+TheThommerd@users.noreply.github.com>
2022-01-24 15:56:06 +0100

    Update Test.js

--- Singles/Test.js
+++ Singles/Test.js
@@ -1577,7 +1577,7 @@ function win(){
 		document.getElementById("passwordsaverR2").value = "BLUE FAIRY";
 		document.getElementById("save").click();
 message004.innerHTML = "You have unlocked the next round. Use the password 'Blue Fairy' to enter." + "</br></br>" + "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";		
-		document.getElementById("message002").style.top = "-265px";
+		document.getElementById("message002").style.top = "-260px";
 		document.getElementById("message004").style.top = "-455px";
 		document.getElementById("message003").style.top = "-275px";
 		document.getElementById("message006").style.top = "-180px";
@@ -1610,7 +1610,7 @@ function lose(){
 		document.getElementById("TotalPoints").value = parseInt(document.getElementById("TotalPoints").value) +  b;
 		document.getElementById("save").click();
             	message004.innerHTML = "<button class=buttons001 onclick=repeat001()>Play again</button>" + "</br></br>" + "<button class=buttons001 onclick=newLoc()>Return to menu</button>";
-		document.getElementById("message002").style.top = "-265px";
+		document.getElementById("message002").style.top = "-260px";
 		document.getElementById("message004").style.top = "-455px";
 		document.getElementById("message003").style.top = "-275px";
 		document.getElementById("message006").style.top = "-180px";

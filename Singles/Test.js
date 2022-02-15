90f44851563a1bcceedc9a0c86b6f4428203de93
TheThommerd <79095774+TheThommerd@users.noreply.github.com>
2022-02-15 19:27:21 +0100

    Update Test.js

--- Singles/Test.js
+++ Singles/Test.js
@@ -7158,7 +7158,7 @@ else if ((stealmc == 0)  && (oppspinfirst == 1)){
 }
 function tweetbutton() {
  document.getElementById("message006").style.top = "-270px";
- document.getElementsByClassName("flash).style.animationName = "none";
+ document.getElementsByClassName("flash").style.animationName = "none";
   html2canvas($('#frame001')[0], {
     width: 950,
     height: 650
@@ -7169,6 +7169,6 @@ function tweetbutton() {
     a.click();
   });
 document.getElementById("message006").style.top = "-120px";
-document.getElementsByClassName("flash).style.animationName = "flash";
+document.getElementsByClassName("flash").style.animationName = "flash";
 window.open("https://twitter.com/intent/tweet?text=" + "I just played a Schmoedown match and won " + b.toString() + "-" + s.toString() + "!" + "%0aTry it out for yourself here: https://thethommerd.github.io/Schmoedown/" + "%0a%23Schmoedown%23MovieTrivia", "_blank");
 }
